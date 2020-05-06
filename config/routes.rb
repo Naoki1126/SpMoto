271dc998eef62e2dbe8c5ea9dc4fd4dfc02fba8c
Naoki1126 <m.naoki85@gmail.com>
2020-05-06 10:52:20 +0000

    [Update]routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,32 @@ Rails.application.routes.draw do
   	registraitions: 'users/registraitions'
   }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #管理者側
+  namespace :admins do
+  	resources :post_images, only: [:index, :show,  :edit, :update, :destoroy]
+  	resources :events, only: [:index, :show, :edit, :update, :destoroy]
+  	resources :users, only: [:index, :show, :edit, :update, :destroy]
+  end
+
+ #会員側
+   root 'homes#top'
+   resources :users, only: [:index, :show, :edit, :update, :destroy]
+  #フォロー関連
+   resources :relationships, only: [:create, :destroy]
+   get 'users/:id/follows' => 'relationships#follows'
+   get 'users/:id/followers' => 'relationships#followers'
+ #投稿関連
+   resources :post_images do
+    resource :post_image_comments, only: [:create, :destroy]
+    resource :post_image_favorites, only: [:create, :destroy]
+   end
+ #イベント関連
+   resources :events do
+   	resource :event_comments, only: [:create, :destroy]
+   	resource :event_participates, only: [:create, :destroy]
+   end
+
+
+
 end

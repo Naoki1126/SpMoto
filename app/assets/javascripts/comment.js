2d002a8e9fb7e93d6b7992cb8ae9f3f1808fe8ad
Naoki1126 <m.naoki85@gmail.com>
2020-06-10 10:38:24 +0000

    [Fix]コメントスクロールjs修正

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,13 +1,10 @@
 $(document).ready(function(){
 	$('.show-comment-count').click(function(){
 		$('.image-show-comment-box').fadeIn();
-	});
-
-	$('.show-comment-count').click(function(){
 		$('html, body').animate(
         	{ scrollTop: $('body').get(0).scrollHeight },
     	);
-    });
+	});
 
 	$('.fa-window-close').click(function(){
 		$('.image-show-comment-box').fadeOut();

828667cb4c3932159fe993db1bc56161f0f8c017
Naoki1126 <m.naoki85@gmail.com>
2020-06-10 08:44:10 +0000

    [Update]コメント、いいね一覧クリックにスクロールイベント追加

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -3,6 +3,12 @@ $(document).ready(function(){
 		$('.image-show-comment-box').fadeIn();
 	});
 
+	$('.show-comment-count').click(function(){
+		$('html, body').animate(
+        	{ scrollTop: $('body').get(0).scrollHeight },
+    	);
+    });
+
 	$('.fa-window-close').click(function(){
 		$('.image-show-comment-box').fadeOut();
 	});

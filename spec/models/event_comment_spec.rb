41314c5714a1214e049e96d0ed341374ee71ee98
Naoki1126 <m.naoki85@gmail.com>
2020-06-11 09:14:17 +0000

    [Fix]js

--- spec/models/event_comment_spec.rb
+++ spec/models/event_comment_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
-RSpec.describe EventComment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe 'EventCommentモデルのテスト', type: :model do
+
+	describe 'アソシエーションのテスト' do
+		context 'Userモデルとの関係' do
+			it 'N:1となっている' do
+				expect(EventComment.reflect_on_association(:user).macro).to eq :belongs_to
+			end
+		end
+
+		context 'Eventモデルとの関係' do
+			it 'N:1となっている' do
+				expect(EventComment.reflect_on_association(:event).macro).to eq :belongs_to
+			end
+		end
+
+		context 'Notificationモデルとの関係' do
+			it '1:Nとなっている' do
+				expect(EventComment.reflect_on_association(:notifications).macro).to eq :has_many
+			end
+		end
+	end
 end

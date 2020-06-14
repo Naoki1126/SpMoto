6fad3377a1b94ed30b130e37fc8f88ae02d333c6
Naoki1126 <m.naoki85@gmail.com>
2020-06-14 11:21:09 +0000

    [Fix]カレンダーのJs

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	$('.fa-calendar').click(function(){
 		$('.carender').fadeToggle();
-		$('.contents-box').fadeToggle();
+		$('.post-image-index-post-box').fadeToggle();
 	});
 });

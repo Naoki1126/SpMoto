f1fdc4bf32370e5f6ddab836b9c1614be5055eeb
Naoki1126 <m.naoki85@gmail.com>
2020-06-18 10:00:16 +0000

    [Update]カレンダーデザイン

--- app/models/event_participate.rb
+++ app/models/event_participate.rb
@@ -2,13 +2,4 @@ class EventParticipate < ApplicationRecord
   belongs_to :user
   belongs_to :event
 
-  #gem simple_calender用
-  def start_time
-    event.date_and_time
-  end
-
-  def end_time
-    event.meetingfinishtime
-  end
-
 end

c48aae862cd30e55095c8f8581580fa84f4201f4
Naoki1126 <m.naoki85@gmail.com>
2020-06-26 11:14:00 +0000

    [add]Rspec request

--- spec/requests/relationships_request_spec.rb
+++ spec/requests/relationships_request_spec.rb
@@ -1,4 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe "Relationships", type: :request do
+
+	let!(:user) { create(:user) }
+
+		 context 'ログインなしでアクセス出来ないこと' do
+			it ' relationships/create' do
+				 post relationships_path
+				expect(response).to have_http_status 302
+			end
+
+			it 'relationships/destroy' do
+				delete relationship_path(Faker::Lorem.characters(number:10))
+				expect(response).to have_http_status 302
+			end
+		end
 end
--- spec/requests/rooms_request_spec.rb
+++ spec/requests/rooms_request_spec.rb
@@ -1,10 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe "Rooms", type: :request do
-  describe "GET /show" do
-    it "returns http success" do
-      get "/rooms/show"
-      expect(response).to have_http_status(:success)
+
+
+  context "ログインなしでアクセス出来ないこと" do
+    it "rooms/index" do
+      get rooms_path
+      expect(response).to have_http_status 302
     end
+
+    it 'rooms/create' do
+    	post rooms_path
+    	expect(response).to have_http_status 302
+    end
+
+    it 'rooms/show' do
+    	get room_path(Faker::Lorem.characters(number:10))
+    	expect(response).to have_http_status 302
+    end
+
   end
 end

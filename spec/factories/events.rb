5b245398ecaa484d1e947e1b054aece05d5e858c
Naoki1126 <m.naoki85@gmail.com>
2020-06-29 06:51:12 +0000

    [add]Rspec system/event_spec

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :event do
-    user_id { FactoryBot.create(:user).id }
+    user
     title { Faker::Lorem.characters(number:10) }
     body { Faker::Lorem.characters(number:20) }
     prefecture_code { 2 }
     capacity { 5 }
-    date_and_time{ DateTime.now }
+    date_and_time{ "Fri, 29 May 2020 11:11:00 JST +09:00" }
     meetingplace{ "11111" }
-    meetingfinishtime{ DateTime.now }
+    meetingfinishtime{ "Fri, 29 May 2020 11:11:00 JST +09:00" }
     latitude{ -34.397 }
     longitude{ 150.644 }
 

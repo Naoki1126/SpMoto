cc6b5462b5fd3339503addf08a8f26def5fa22fa
Naoki1126 <m.naoki85@gmail.com>
2020-06-30 06:50:48 +0000

    [Update]spec/system/event_speck

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,6 +1,20 @@
 FactoryBot.define do
-  factory :event do
+  factory :event, class:Event do
     user
+    title { Faker::Lorem.characters(number:10) }
+    body { Faker::Lorem.characters(number:20) }
+    prefecture_code { 2 }
+    capacity { 5 }
+    date_and_time{ Time.now }
+    meetingplace{ "11111" }
+    meetingfinishtime{ Time.now }
+    latitude{ -34.397 }
+    longitude{ 150.644 }
+
+  end
+
+  factory :test_event2, class:Event do
+    user { FactoryBot.create(:test_user2)}
     title { Faker::Lorem.characters(number:10) }
     body { Faker::Lorem.characters(number:20) }
     prefecture_code { 2 }

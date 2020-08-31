46fac179842060c525c6f209d8d9c713bdeafddd
Naoki1126 <m.naoki85@gmail.com>
2020-08-31 13:28:40 +0000

    [capistrano]

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.14.1"
 
 set :application, "SpMoto"
-set :repo_url, "https://github.com/Naoki1126/SpMoto.git"
+set :repo_url, "git@github.com:Naoki1126/SpMoto.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
